8b5332b7c93f258270a7f9f6c92abc132371638d
lxy <a1242753955@163.com>
2024-11-13 12:01:54 +0800

    苍穹外卖-Java操作Redis初步

---
+++ sky-server/src/test/java/com/sky/test/SpringDataRedisTest.java
@@ -0,0 +1,63 @@
+package com.sky.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class SpringDataRedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testRedisTemplate(){
+        System.out.println(redisTemplate);
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        ListOperations listOperations = redisTemplate.opsForList();
+        SetOperations setOperations = redisTemplate.opsForSet();
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+    }
+
+    /*操作字符串类型的数据*/
+    @Test
+    public void testString(){
+        // set get setex setnx
+        redisTemplate.opsForValue().set("city","北京");
+        String city = (String) redisTemplate.opsForValue().get("city");
+        System.out.println(city);
+        redisTemplate.opsForValue().set("code","1234",3, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().setIfAbsent("lock","1");
+        redisTemplate.opsForValue().setIfAbsent("lock","2");
+    }
+
+    /*操作哈希类型的数据*/
+    @Test
+    public void testHash(){
+        // hset hget hdel hkeys hvals
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        hashOperations.put("100","name","tom");
+        hashOperations.put("100","age","20");
+
+        String name = (String) hashOperations.get("100", "name");
+        System.out.println(name);
+
+        Set keys = hashOperations.keys("100");
+        System.out.println(keys);
+
+        List values = hashOperations.values("100");
+        System.out.println(values);
+
+        hashOperations.delete("100","age");
+    }
+
+    /*操作列表类型的数据*/
+    /*操作集合类型的数据*/
+    /*操作有序集合类型的数据*/
+    /*通用命令操作*/
+}
