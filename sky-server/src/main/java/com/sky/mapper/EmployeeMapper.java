5847349f99d3e8ce1d7140aa9882735b48cfb59d
lxy <a1242753955@163.com>
2024-11-14 09:29:02 +0800

    苍穹外卖-公共字段自动填充1

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -21,12 +23,14 @@ public interface EmployeeMapper {
     /*插入员工数据*/
     @Insert("insert into employee(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "VALUES (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
 
     /*分页查询*/
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     /*根据主键动态修改属性*/
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /*根据id查询员工信息*/
